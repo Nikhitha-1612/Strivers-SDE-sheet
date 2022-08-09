3ef5907633308d29cf91b803bd441acf1679d552
Nikhitha-1612 <96165205+Nikhitha-1612@users.noreply.github.com>
2022-08-09 19:36:16 +0530

    Update 3. Longest Substring Without Repeating Characters.java

--- Day-4/3. Longest Substring Without Repeating Characters.java
+++ Day-4/3. Longest Substring Without Repeating Characters.java
@@ -1,3 +1,26 @@
+//Brute Force
+class Solution {
+    public int lengthOfLongestSubstring(String str) {
+      if(str.length()==0)
+          return 0;
+        int maxi =0;
+        for(int i=0;i<str.length();i++){
+            HashSet<Character> hs = new HashSet<Character>();
+            for(int j=i;j<str.length();j++){
+                if(hs.contains(str.charAt(j))){
+                 maxi = Math.max(maxi, j-i);
+                break;
+                }
+               hs.add(str.charAt(j));
+            }
+        }
+        return maxi;
+    }
+}
+//Time complexity : O(n^2)
+//Space Complexity : O(n)
+
+//optmized solution
 class Solution {
     public int lengthOfLongestSubstring(String str) {
       if(str.length()==0)
@@ -18,3 +41,5 @@ class Solution {
         return maxi;
     }
 }
+//Time Complexity : O(2N)
+//Space Complexity : O(N)
